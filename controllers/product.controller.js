6238ab1311f303a7ff412f45828ce602470de61a
md-suhag <abdussalamsuhag104@gmail.com>
2025-03-16 10:57:11 +0600

    Implement getting single product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product.model");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const QueryBuilder = require("../utils/queryBuilder");
 
@@ -14,6 +15,7 @@ const getAllProducts = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+    message: "Products successfully fetched",
     count: products.length,
     total: totalProducts,
     totalPages: Math.ceil(totalProducts / (req.query.limit || 10)),
@@ -22,6 +24,23 @@ const getAllProducts = catchAsync(async (req, res, next) => {
   });
 });
 
+const getSingleProduct = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findOne({ _id: id });
+
+  if (!product) {
+    return next(new AppError("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Product successfully fetched",
+    product,
+  });
+});
+
 module.exports = {
   getAllProducts,
+  getSingleProduct,
 };
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/product.controller");
+const {
+  getAllProducts,
+  getSingleProduct,
+} = require("../controllers/product.controller");
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
+router.get("/:id", getSingleProduct);
 
 module.exports = router;
